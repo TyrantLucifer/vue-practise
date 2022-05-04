8e71b336fe8e658cbd8bba23e18e6c3a0877508a
tyrantlucifer <tyrantlucifer@gmail.com>
2022-05-05 00:57:50 +0800

    add day-07 notes

---
+++ day-07/todo_list/src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :receive="receive"></MyHeader>
+        <MyList :todos="todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"></MyList>
+        <MyFooter :todos="todos"></MyFooter>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyList from './components/MyList.vue'
+import MyFooter from './components/MyFooter.vue'
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyList,
+    MyFooter,
+    MyHeader
+  },
+  data() {
+    return {
+      todos: [
+        {id: 1, title: '吃饭', done: true},
+        {id: 2, title: '睡觉', done: false},
+        {id: 3, title: '打游戏', done: true},
+      ]
+    }
+  },
+  methods: {
+    // 添加一个todo
+    receive(todoObj) {
+      if (!todoObj.title.trim()) {
+        alert("todo内容不能为空")
+      } else {
+        this.todos.push(todoObj)
+      }
+    },
+    //更改todo的状态
+    checkTodo(id) {
+      this.todos.forEach(todo => {
+        if (todo.id === id) {
+          todo.done = !todo.done
+        }
+      })
+    },
+    //删除todo
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
+  }
+}
+</script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
---
+++ day-07/todo_list/src/components/MyFooter.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="todo-footer">
+    <label>
+      <input type="checkbox"/>
+    </label>
+    <span>
+          <span>已完成 {{getCount.done}}</span> / 全部 {{getCount.total}}
+        </span>
+    <button class="btn btn-danger">清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyFooter',
+  computed: {
+    getCount() {
+      const total = this.todos.length
+      const done = this.todos.filter(todo => todo.done).length
+      return {total: total, done: done}
+    }
+  },
+  props: ['todos']
+}
+</script>
+
+<style>
+/*footer*/
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
